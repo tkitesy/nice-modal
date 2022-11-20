c69c82c080766d137f75eaddc80fc0f59b433d31
Yang Jun <yangjun23@163.com>
2022-11-20 18:11:30 +0800

    🎉 init: base code

---
+++ src/index.tsx
@@ -0,0 +1,112 @@
+import {
+  Component,
+  ConcreteComponent,
+  defineComponent,
+  InjectionKey,
+  DefineComponent,
+  inject,
+  shallowRef,
+  provide,
+  ShallowRef,
+} from "vue";
+
+export type VueProps<T> = T extends ConcreteComponent<infer Props>
+  ? Props
+  : T extends Component<infer Props>
+  ? Props
+  : any;
+
+
+const defaultCallback = (args: unknown) => {};
+
+type NiceModalContext = {
+  visibleRef: ShallowRef<boolean>;
+  show():Promise<unknown>;
+  hide():void;
+  remove():void;
+  resolve(args:unknown):void;
+  reject(args:unknown):void;
+}
+
+export function create<C extends Component, Props extends {} = VueProps<C>>(
+  Comp: C
+) {
+  const argsRef = shallowRef({});
+  const removedRef = shallowRef(true);
+  const visibleRef = shallowRef(false);
+  let promise: Promise<unknown> | null = null;
+  let resolveCallback = defaultCallback;
+  let rejectCallback = defaultCallback;
+
+  function show(args?: Props) {
+    visibleRef.value = true;
+    removedRef.value = false;
+    if (args) {
+      argsRef.value = args;
+    }
+    if (!promise) {
+      promise = new Promise<unknown>((_resolve, _reject) => {
+        resolveCallback = _resolve;
+        rejectCallback = _reject;
+      });
+    }
+    return promise;
+  }
+  function hide() {
+    argsRef.value = {};
+    promise = null;
+    visibleRef.value = false;
+  }
+  function resolve(args: unknown) {
+    resolveCallback(args);
+  }
+  function reject(args: unknown) {
+    rejectCallback(args);
+  }
+  function remove() {
+    removedRef.value = true;
+  }
+
+  const Placeholder = defineComponent(function () {
+
+    provide(NiceModalContext, {
+      visibleRef,
+      show,
+      hide,
+      remove,
+      resolve,
+      reject,
+    })
+    return function render() {
+      const args = argsRef.value;
+      const removed = removedRef.value;
+      return !removed && <Comp {...args} />;
+    };
+  });
+
+  const service = {
+    show,
+    hide,
+    remove,
+  };
+
+  Placeholder.service = service
+
+  return Placeholder as DefineComponent<Partial<Props>> & {
+    service: typeof service;
+  };
+}
+
+
+
+const NiceModalContext: InjectionKey<NiceModalContext> = Symbol("NiceModalContext");
+
+export function useModal() {
+  const modal = inject(NiceModalContext);
+
+  if (!modal) {
+    throw new Error("useModal must use as child of NiceModal Context");
+  }
+
+  return modal;
+}
